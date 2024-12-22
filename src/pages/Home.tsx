1171aed07a924cb49575666fc3fb4f3fce1396f5
GustavoCunhaLacerda <gustavocunhalacerda@gmail.com>
2024-12-22 20:51:40 -0300

    gamelife control

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,98 +1,137 @@
 import { useEffect, useState } from "react";
 import database from "../data/database";
 import Fireworks from "@fireworks-js/react";
+import Swal from "sweetalert2";
 
 export default function Home() {
-  const bingoItems = database.getNineFlushedItems();
+  const [bingoItems, setBingoItems] = useState(database.getNineFlushedItems());
 
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-10 bg-gray-50">
-      <div className="w-full p-6 text-center">
+      <header className="w-full p-6 text-center">
         <h1 className="font-sans text-4xl font-semibold">Bingo SROMC</h1>
-      </div>
+      </header>
 
-      <div>
-        <Table bingoItems={bingoItems}></Table>
-      </div>
+      <main>
+        <Table bingoItems={bingoItems} setBingoItems={setBingoItems} />
+      </main>
     </div>
   );
 }
 
-function Table({ bingoItems }: { bingoItems: Array<string> }) {
+function Table({
+  bingoItems,
+  setBingoItems,
+}: {
+  bingoItems: Array<string>;
+  setBingoItems: React.Dispatch<React.SetStateAction<any[]>>;
+}) {
   const [selectedItems, setSelectedItems] = useState<Array<number>>([]);
   const [winner, setWinner] = useState<boolean>(false);
+  const [viewMode, setViewMode] = useState<boolean>(false);
 
-  function addSelectedItem(itemIndex: number) {
-    setSelectedItems((curr) => [...curr, itemIndex]);
+  const addSelectedItem = (itemIndex: number) => {
+    if (!selectedItems.includes(itemIndex)) {
+      setSelectedItems((curr) => [...curr, itemIndex]);
+    }
+  };
+
+  function resetCurrentGame() {
+    setSelectedItems([]);
+    setWinner(false);
+    setViewMode(false);
+  }
+
+  function restartGame() {
+    resetCurrentGame();
+    setBingoItems(database.getNineFlushedItems());
+    setWinner(false);
+    setViewMode(false);
   }
 
   useEffect(() => {
-    if (
-      (selectedItems.includes(0) &&
-        selectedItems.includes(1) &&
-        selectedItems.includes(2)) ||
-      (selectedItems.includes(3) &&
-        selectedItems.includes(4) &&
-        selectedItems.includes(5)) ||
-      (selectedItems.includes(6) &&
-        selectedItems.includes(7) &&
-        selectedItems.includes(8)) ||
-      (selectedItems.includes(0) &&
-        selectedItems.includes(3) &&
-        selectedItems.includes(6)) ||
-      (selectedItems.includes(1) &&
-        selectedItems.includes(4) &&
-        selectedItems.includes(7)) ||
-      (selectedItems.includes(2) &&
-        selectedItems.includes(5) &&
-        selectedItems.includes(8)) ||
-      (selectedItems.includes(0) &&
-        selectedItems.includes(4) &&
-        selectedItems.includes(8))
-    ) {
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    const isWinner = winningPatterns.some((pattern) =>
+      pattern.every((index) => selectedItems.includes(index)),
+    );
+
+    if (isWinner) {
       setWinner(true);
+
+      Swal.fire({
+        title: "BINGO!",
+        confirmButtonText: "Reiniciar",
+        backdrop: false,
+        showCloseButton: true,
+      }).then((res) => {
+        if (res.isConfirmed) {
+          restartGame();
+        } else {
+          setWinner(false);
+          setViewMode(true);
+        }
+      });
     }
   }, [selectedItems]);
 
   return (
-    <div className="z-10 grid grid-cols-3 gap-3">
-      {bingoItems.map((item: string, itemIndex: number) => {
-        if (selectedItems.includes(itemIndex)) {
-          return (
-            <div
-              key={Math.random()}
-              className="size-[120px] rounded-md border border-blue-800 bg-blue-800 p-4 text-sm font-semibold text-white hover:cursor-pointer hover:bg-white hover:text-gray-900"
-              onClick={() => addSelectedItem(itemIndex)}
-            >
-              {item}
-            </div>
-          );
-        }
-
-        return (
+    <div>
+      <div className="relative z-10 grid grid-cols-3 gap-3">
+        {bingoItems.map((item: string, itemIndex: number) => (
           <div
-            key={Math.random()}
-            className="size-[120px] rounded-md border border-blue-800 bg-white p-4 text-sm font-semibold text-gray-900 hover:cursor-pointer hover:bg-blue-800 hover:text-white"
-            onClick={() => addSelectedItem(itemIndex)}
+            key={itemIndex}
+            className={`size-[120px] cursor-pointer rounded-md border p-4 text-xs font-semibold transition-colors md:size-[200px] md:text-base ${
+              selectedItems.includes(itemIndex)
+                ? "border-blue-800 bg-blue-800 text-white"
+                : `border-blue-800 bg-white text-gray-900 ${!viewMode && "hover:bg-blue-600 hover:text-white"}`
+            }`}
+            onClick={() => !viewMode && addSelectedItem(itemIndex)}
           >
             {item}
           </div>
-        );
-      })}
+        ))}
 
-      {winner && (
-        <Fireworks
-          options={{ opacity: 0.5 }}
-          style={{
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            position: "fixed",
-            background: "#00000",
-          }}
-        />
-      )}
+        {winner && (
+          <Fireworks
+            options={{ opacity: 0.5 }}
+            style={{
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              position: "fixed",
+              background: "rgba(0, 0, 0, 0.2)",
+            }}
+          />
+        )}
+      </div>
+
+      <div className="flex w-full gap-2 pt-2">
+        <button
+          className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={restartGame}
+        >
+          Nova Cartela
+        </button>
+        {selectedItems.length > 0 && (
+          <button
+            className="rounded-md bg-red-600 px-4 py-2 text-white hover:bg-red-700"
+            onClick={resetCurrentGame}
+          >
+            Reiniciar
+          </button>
+        )}
+      </div>
     </div>
   );
 }
